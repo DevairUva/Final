11a83c2e6ba4f36af08744ce9ad617a021b1467c
DEVair Martins <devair.martins@gmail.com>
2023-03-23 19:45:05 -0300

    loguin
    tentando centralizar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,13 +8,16 @@ function Login() {
                 <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
                 <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                    <label for="floatingPassword">Password</label>
+                <div>
+                    a
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <label for="floatingInput">Email address</label><br />
+                    </div><br />
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                        <label for="floatingPassword">Password</label>
+                    </div><br />
                 </div>
 
                 <div class="checkbox mb-3">
@@ -22,7 +25,7 @@ function Login() {
                         <input type="checkbox" value="remember-me" /> Remember me
                     </label>
                 </div>
-                <button class="btn btn-lg btn-primary form-floating" type="submit">Sign in</button>
+                <button class="btn btn-lg btn-primary" type="submit">Sign in</button>
                 <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
             </form>
 
