276ed8ce8326971dd1e1d91c6b76262e2ace4191
carolrc <carol_26_01@hotmail.com>
2023-03-23 21:26:49 -0300

    Atualização

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -67,21 +67,18 @@ function Home() {
           <div class="row">
             <div class="col-lg-4">
               <img class="rounded-circle" width="140" height="140" src={edu} alt="" />
-              {/* <svg class="rounded-circle" width="140" height="140" xmlns="https://www.correio24horas.com.br/fileadmin/_processed_/c/3/csm_24102018_baz_sommelier_de_aguas_cred_divulgacao_shutterstock_1_aff62af31d.jpg" aria-label="Placeholder: 140x140" preserveAspectRatio="" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
               <h2 class="fw-normal">Eduardo Paes</h2>
               <p>Prefeito da cidade do Rio de Janeiro.</p>
               <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
             </div>
             <div class="col-lg-4">
               <img class="rounded-circle" width="140" height="140" src={chico} alt="" />
-              {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
               <h2 class="fw-normal">Chicão Bulhões</h2>
               <p>Secretário municipal de Desenvolvimento Econômico, Inovação e Simplificação.</p>
               <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
             </div>
             <div class="col-lg-4">
               <img class="rounded-circle" width="140" height="140" src={marcel} alt="" />
-              {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
               <h2 class="fw-normal">Marcel Balassiano</h2>
               <p>Subsecretário municipal de Desenvolvimento Econômico e Inovação.</p>
               <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
@@ -92,28 +89,27 @@ function Home() {
 
       <div class="background">
         <div class="container marketing1">
-          <hr class="featurette-divider" />
+          < div class="featurette-divider" />
 
           <div class="row featurette">
-            <div class="col-md-7">
+            <div class="col-md-6">
               <h2 class="featurette-heading fw-normal lh-1">Projeto Programadores Cariocas capacita jovens do Rio para a área de TI.</h2>
               <p class="lead">Iniciativa da Prefeitura oferece bolsas, auxílio financeiro e computadores para formar profissionais.</p>
             </div>
             <div class="col-md-5">
               <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src={prog} alt="" />
-              {/* <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> */}
             </div>
           </div>
         </div>
       </div>
 
       <div class="container marketing2">
-        <hr class="featurette-divider" />
+        <div class="featurette-divider" />
 
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
             <h2 class="featurette-heading fw-normal lh-1">Estão sendo ofertadas 200 Bolsas Integrais e 1000 parciais.</h2>
-            <p class="lead">Caso o aluno se qualifique para as bolsas integrais não deverá pagar nada. No caso das bolsas parciais, o aluno só deve efetuar o pagamento caso esteja empregado na área de tecnologia com rendimentos superiores a R$2200.</p>
+            <p class="lead2">Caso o aluno se qualifique para as bolsas integrais não deverá pagar nada. No caso das bolsas parciais, o aluno só deve efetuar o pagamento caso esteja empregado na área de tecnologia com rendimentos superiores a R$2200.</p>
           </div>
           <div class="col-md-5 order-md-1">
             <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -122,28 +118,32 @@ function Home() {
       </div>
 
       <div class="background">
-      <div class="container marketing3">
-        <hr class="featurette-divider" />
+        <div class="container marketing3">
+          <div class="featurette-divider" />
 
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading fw-normal lh-1">A meta é formar cinco mil profissionais nos próximos três anos</h2>
-            <p class="lead">O Programadores Cariocas também vai oferecer dois auxílios para todos os jovens matriculados: um financeiro de R$ 500 por mês e um computador ao concluírem o curso.</p>
-          </div>
-          <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+          <div class="row featurette">
+            <div class="col-md-6">
+              <h2 class="featurette-heading fw-normal lh-1">A meta é formar cinco mil profissionais nos próximos três anos</h2>
+              <p class="lead">O Programadores Cariocas também vai oferecer dois auxílios para todos os jovens matriculados: um financeiro de R$ 500 por mês e um computador ao concluírem o curso.</p>
+            </div>
+            <div class="col-md-5">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            </div>
           </div>
         </div>
       </div>
-      </div>
+
+      <div class="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+</div>
 
       <div class="container marketing4">
-        <hr class="featurette-divider" />
+        <div class="featurette-divider" />
 
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
             <h2 class="featurette-heading fw-normal lh-1">A capacitação é oferecida por duas empresas: Senac, em parceria com a Resília, e a Generation.</h2>
-            <p class="lead">O curso acontecerá em oito bairros do Rio: Madureira, Riachuelo, Irajá, Bonsucesso, Campo Grande, Centro, Bangu e Jacarepaguá.</p>
+            <p class="lead2">O curso acontecerá em oito bairros do Rio: Madureira, Riachuelo, Irajá, Bonsucesso, Campo Grande, Centro, Bangu e Jacarepaguá.</p>
           </div>
           <div class="col-md-5 order-md-1">
             <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -151,6 +151,8 @@ function Home() {
         </div>
       </div>
     </div>
+    
+
 
 
   );
