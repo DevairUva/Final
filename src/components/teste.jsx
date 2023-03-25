97a0e0f901807bda3b8f2654cd3ee00941bb83e7
DEVair Martins <devair.martins@gmail.com>
2023-03-25 17:18:50 -0300

    testando
    tentando pegar as informações do bd

--- src/components/teste.jsx
+++ src/components/teste.jsx
@@ -1,4 +1,4 @@
-import Card from "react-bootstrap/Card";
+import Card from 'react-bootstrap/Card';
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -7,49 +7,50 @@ function Teste() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
-//   useEffect(()=>{
-//       const option = { method: 'GET', url : 'http://localhost:3000/usuario'};
-//       axios.request(options).then(function (response){
-//           setNome(response.data[0].nome)
-//           setEmail(response.data[0].email)
-//           setSenha(response.data[0].senha)
-//       }).catch(function(error){
-//           console.error(error);
-//       });
-//   },[])
+  useEffect(() => {
+    const options = { method: 'GET', url: 'http://localhost:3000/usuario' };
+    axios.request(options).then(function (response) {
+      setNome(response.data[0].nome)
+      setEmail(response.data[0].email)
+      setSenha(response.data[0].senha)
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
 
   return (
-    // <div class="container topoLog">
-    //   <div class="mb-3">
-    //     <label for="exampleFormControlTextarea1" class="form-label">
-    //       Example textarea
-    //     </label>
-    //     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea>
-    //     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea>
-    //     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea>
-    //   </div>
-    // </div>
+    <div class="container topoLog">
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">
+          Example textarea
+        </label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={nome}></textarea><br />
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">{email}</textarea>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">{senha}</textarea>
+        <p>{nome}</p>
+      </div>
 
-    <Card class="container topoLog">
-      <Card.Header>{nome} a</Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-        <div class="mb-3">
-         <label for="exampleFormControlTextarea1" class="form-label">
-           Example textareaa
-         </label>
-         {/* <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br/>
-         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br/>
-         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea> */}
+      <Card class="container topoLog">
+        <Card.Header>{nome} a</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">
+                Example textareaa
+              </label>
+              {/* <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br/>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br/>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea> */}
 
-         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">{nome}</textarea><br/>
-         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br/>
-         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br />
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea><br />
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">a</textarea>
 
-       </div>
-        </blockquote>
-      </Card.Body>
-    </Card>
+            </div>
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 export default Teste;
