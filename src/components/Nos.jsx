e4557da9e6c2e19f2829309c527f57b83f50439b
carolrc <carol_26_01@hotmail.com>
2023-03-27 02:17:24 -0300

    Atualização

--- src/components/Nos.jsx
+++ src/components/Nos.jsx
@@ -1,9 +1,12 @@
+import '../styles/nos.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import isa from '../assets/isa.png'
 import carol from '../assets/carol.png'
 import dev from '../assets/dev.png'
 import wash from '../assets/wash.png'
+import linkedin from '../assets/linkedin.png'
+import git from '../assets/git.png'
 
 
 
@@ -18,26 +21,32 @@ function Nos() {
                     <div className="col-lg-4">
                         <img className="rounded-circle" width="140" height="140" src={carol} alt="" />
                         <h2 className="fw-normal">Carolina Rocha</h2>
-                        <p>É o atual prefeito do Rio de Janeiro, cargo que ocupou anteriormente de 2009 a 2017.</p>
-                        {/* <p><a className="btn btn-info" href="#">View details &raquo;</a></p> */}
+                        <p>Desenvolvedora front e designer</p>
+                        <img className="bd-placeholder-img" width="30" height="30" src={git} alt=""  />
+                        <img className="bd-placeholder-img2" width="25" height="25" src={linkedin} alt=""  />
+                       
                     </div>
                     <div className="col-lg-4">
                         <img className="rounded-circle" width="140" height="140" src={dev} alt="" />
                         <h2 className="fw-normal">Devair Martins</h2>
-                        <p>Secretário municipal de Desenvolvimento Econômico, Inovação e Simplificação.</p>
-                        {/* <p><a className="btn btn-info" href="#">View details &raquo;</a></p> */}
+                        <p>Desenvolvedor front e back end</p>
+                        <img className="bd-placeholder-img" width="30" height="30" src={git} alt=""  />
+                        <img className="bd-placeholder-img2" width="25" height="25" src={linkedin} alt=""  />
+                        
                     </div>
                     <div className="col-lg-4">
                         <img className="rounded-circle" width="140" height="140" src={isa} alt="" />
                         <h2 className="fw-normal">Isabela Alves</h2>
-                        <p>É o atual prefeito do Rio de Janeiro, cargo que ocupou anteriormente de 2009 a 2017.</p>
-                        {/* <p><a className="btn btn-info" href="#">View details &raquo;</a></p> */}
+                        <p>Desenvolvedora front e back end</p>
+                        <img className="bd-placeholder-img" width="30" height="30" src={git} alt=""  />
+                        <img className="bd-placeholder-img2" width="25" height="25" src={linkedin} alt=""  />
                     </div>
                     <div className="col-lg-4">
                         <img className="rounded-circle" width="140" height="140" src={wash} alt="" />
                         <h2 className="fw-normal">Washignton Ligel</h2>
-                        <p>Secretário municipal de Desenvolvimento Econômico, Inovação e Simplificação.</p>
-                        {/* <p><a className="btn btn-info" href="#">View details &raquo;</a></p> */}
+                        <p>Desenvolvedor front e back end</p>
+                        <img className="bd-placeholder-img" width="30" height="30" src={git} alt=""  />
+                        <img className="bd-placeholder-img2" width="25" height="25" src={linkedin} alt=""  />
                     </div>
                 
                 </div>
