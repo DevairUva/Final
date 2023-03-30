b492f0faf8382060086ad9d86773e1fbfdb677f2
carolrc <carol_26_01@hotmail.com>
2023-03-30 03:18:39 -0300

    atualizaçao

--- src/components/Inscricao.jsx
+++ src/components/Inscricao.jsx
@@ -3,6 +3,8 @@ import emailjs from '@emailjs/browser'
 import programador from '../assets/15.png'
 import logo from '../assets/logo.png'
 import { useState } from 'react'
+import background from '../assets/fundo2.png'
+import inscricao from '../assets/inscricao.png'
 
 function Inscricao() {
 
@@ -26,135 +28,93 @@ function Inscricao() {
     }
 
     return (
-        <div className="container topoInsc">
-            <div className="py-5 text-center">
-                <img className="d-block mx-auto mb-4" src={logo} alt="" width="150" height="90" />
-                <h2>Inscreva-se</h2>
-                <p className="lead">Faça a sua inscrição e se torne um programador Full Stack.</p>
+        <div className="backs">
+            <div className="col-md-4">
+                <img className="bd-placeholder-img" id="acesso" width="450" height="450" src={inscricao} alt="" />
             </div>
-
-            <div className="row g-5">
-                <div className="col-md-5 col-lg-4 order-md-last">
-                    <img className="bd-placeholder-img" width='430px' height='750px' src={programador} alt="" />
+            <div className="container topoInsc">
+                <div className="py-5 text-center">
+                    {/* <p className="lead3">Faça a sua inscrição e se torne um programador Full Stack.</p> */}
                 </div>
 
-                <div className="col-md-7 col-lg-6">
-                    <h4 className="mb-3">Faça seu cadastro</h4>
-                    <form onSubmit={enviarEmail}>
-                        <div className="row g-3">
+                <div className="row g-5">
+                    <div className="col-md-5 col-lg-4 order-md-last">
+                        {/* <img className="bd-placeholder-img" width='430px' height='750px' src={programador} alt="" /> */}
+                    </div>
 
-                            <div className="col-sm-6">
-                                <label for="username" className="form-label">Nome</label>
-                                <input type="text" className="form-control" id="firstname" required onChange={(e) => setName(e.target.value)} />
-                                <div className="invalid-feedback">
-                                    Your username is required.
-                                </div>
-                            </div>
+                    <div className="col-md-7 col-lg-6">
+                        <h4 className="mb-3"   id="principal">Faça seu cadastro</h4>
+                        <form onSubmit={enviarEmail}>
+                            <div className="row g-3">
 
+                                <div class="d-flex flex-row">
+                                    <div class="col-sm-5" id="nomeform">
+                                        <label for="username" class="form-label">Nome</label>
+                                        <input type="text" class="form-control" id="firstname" required onChange={(e) => setName(e.target.value)} />
+                                        <div class="invalid-feedback">
+                                            Your username is required.
+                                        </div>
+                                    </div>
 
-                            <div className="col-sm-6">
-                                <label for="username" className="form-label">Sobrenome</label>
-                                <input type="text" className="form-control" id="lastname" required />
-                                <div className="invalid-feedback">
-                                    Your username is required.
-                                </div>
-                            </div>
+                                    <div class="col-sm-5" id="nomeform">
+                                        <label for="username" class="form-label">Sobrenome</label>
+                                        <input type="text" class="form-control" id="lastname" required />
+                                        <div class="invalid-feedback">
+                                            Your username is required.
+                                        </div>
+                                    </div>
 
-                            <div className="col-12">
-                                <label for="username" className="form-label">Nome de usuário</label>
-                                <div className="input-group has-validation">
-                                    <span className="input-group-text">@</span>
-                                    <input type="text" className="form-control" id="username" placeholder="Usuario" required />
-                                    <div className="invalid-feedback">
-                                        Your username is required.
+                                    <div class="col-10" id="nomeform" >
+                                        <label for="username" class="form-label">Nome de usuário</label>
+                                        <div class="input-group has-validation">
+                                            <span class="input-group-text">@</span>
+                                            <input type="text" class="form-control" id="username" placeholder="Usuario" required />
+                                            <div class="invalid-feedback">
+                                                Your username is required.
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="col-12">
-                                <label for="email" className="form-label">E-mail <span className="text-muted"></span></label>
-                                <input type="email" className="form-control" id="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
-                                <div className="invalid-feedback">
-                                    Please enter a valid email address for shipping updates.
+
+                                <div class="d-flex flex-row">
+                                    <div class="col-10" id="nomeform">
+                                        <label for="email" class="form-label">E-mail <span class="text-muted"></span></label>
+                                        <input type="email" class="form-control" id="email" placeholder="seu@email.com" onChange={(e) =>
+                                            setEmail(e.target.value)} />
+                                        <div class="invalid-feedback">
+                                            Please enter a valid email address for shipping updates.
+                                        </div>
+                                    </div>
+
+                                    <div class="col-10" id="nomeform">
+                                        <label for="address" class="form-label">Endereço</label>
+                                        <input type="text" class="form-control" id="address" placeholder="rua a, 123 - bairro" required />
+                                        <div class="invalid-feedback">
+                                            Please enter your shipping address.
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
 
-                            <div className="col-12">
-                                <label for="address" className="form-label">Endereço</label>
-                                <input type="text" className="form-control" id="address" placeholder="rua a, 123 - bairro" required />
-                                <div className="invalid-feedback">
-                                    Please enter your shipping address.
+                            <div className="d-flex align-items-center">
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="save-info" />
+                                    <label className="form-check-label" for="save-info">Salvar esta informação para a próxima vez.</label>
                                 </div>
+                                <hr className="my-4 flex-grow-1" />
+                                <button className="btn btn-primary btn-lg btn-block" id="botaozao" type="submit">CONCLUIR CADASTRO</button>
+
                             </div>
 
-                            <div className="col-12">
-                                <label for="address2" className="form-label">Complemento <span className="text-muted">(Opcional)</span></label>
-                                <input type="text" className="form-control" id="address2" placeholder="Casa, apartamento, etc." />
-                            </div>
+                        </form>
+                    </div>
 
-                            {/* <div className='col-md-5'>
-                                <label for="country" className="form-label">País</label>
-                                <select className="form-select" id="country" required>
-                                    <option value="">Selecione...</option>
-                                    <option>Brasil</option>
-                                </select>
-                                <div className="invalid-feedback">
-                                    Por favor, selecione um país válido.
-                                </div>
-                            </div>
-
-                            <div className="col-md-5">
-                                <label for="country" className="form-label">País</label>
-                                <select className="form-select" id="country" required>
-                                    <option value="">Selecione...</option>
-                                    <option>Brasil</option>
-                                </select>
-                                <div className="invalid-feedback">
-                                    Por favor, selecione um país válido.
-                                </div>
-                            </div>
-
-                            <div className="col-md-4">
-                                <label for="state" className="form-label">Estado</label>
-                                <select className="form-select" id="state" required>
-                                    <option value="">Selecione...</option>
-                                    <option>Rio de Janeiro</option>
-                                </select>
-                                <div className="invalid-feedback">
-                                    Por favor, selecione um Estado válido.
-                                </div>
-                            </div>
-
-                            <div className="col-md-3">
-                                <label for="zip" className="form-label">CEP</label>
-                                <input type="text" className="form-control" id="zip" placeholder="" required />
-                                <div className="invalid-feedback">
-                                    É necessário informar o CEP.
-                                </div>
-                            </div> */}
-                        </div>
-
-                        <hr className="my-4" />
-
-                        {/* <div className="form-check">
-                            <input type="checkbox" className="form-check-input" id="same-address" />
-                            <label className="form-check-label" for="same-address">O endereço de entrega é o mesmo do meu endereço de cobrança.</label>
-                        </div> */}
-
-                        <div className="form-check">
-                            <input type="checkbox" className="form-check-input" id="save-info" />
-                            <label className="form-check-label" for="save-info">Salvar esta informação para a próxima vez.</label>
-                        </div>
-
-                        <hr className="my-4" />
-
-                        <button className="w-100 btn btn-primary btn-lg" type="submit">Concluir seu cadastro</button>
-                    </form>
                 </div>
 
-            </div>
-
-        </div >
+            </div >
+        </div>
     )
 }
 export default Inscricao;
